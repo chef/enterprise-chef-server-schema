9ba079332ee64eb0e6c18af0296e5e072143d19f
danielsdeleo <dan@getchef.com>
2015-02-27 08:31:24 -0800

    Drop constraint before dropping column

--- revert/policies-revision-id.sql
+++ revert/policies-revision-id.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-ALTER TABLE policies DROP COLUMN revision_id CASCADE;
 ALTER TABLE policies DROP CONSTRAINT unique_name_and_revision_id;
+ALTER TABLE policies DROP COLUMN revision_id CASCADE;
 
 COMMIT;
